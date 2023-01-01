2194c0fa675322f7e3ae366ccb749e5a22cfd12b
Anna <annkrv80@yandex.ru>
2023-01-01 15:19:48 +0700

    Исследования

--- Reserch.java
+++ Reserch.java
@@ -3,40 +3,39 @@ import java.util.HashSet;
 import java.util.Set;
 
 public class Reserch {
-    ArrayList<String> result= new ArrayList<>();
-    Set<String> res= new HashSet<>();
+    ArrayList<String> result = new ArrayList<>();
+    Set<String> res = new HashSet<>();
     ArrayList<Node> tree;
 
-    public Reserch(GeoTree geoTree){
-        tree=geoTree.getTree();
+    public Reserch(GeoTree geoTree) {
+        tree = geoTree.getTree();
     }
 
-    public ArrayList<String> spend(Person p, Relationship re){
-        for(Node t : tree){
-            if(t.p1.fullName==p.fullName && t.re==re){
+    public ArrayList<String> spend(Person p, Relationship re) {
+        for (Node t : tree) {
+            if (t.p1.fullName == p.fullName && t.re == re) {
                 result.add(t.p2.fullName);
             }
         }
         return result;
     }
 
-    public void whoIsOlder(Person p){
-        for(Node t : tree){
-            if(t.p1.getAge()>p.getAge()){
-               res.add(t.p1.fullName);
-            }                                   
-        }    
-      System.out.println(res);
+    public void whoIsOlder(Person p) {
+        for (Node t : tree) {
+            if (t.p1.getAge() > p.getAge()) {
+                res.add(t.p1.fullName);
+            }
+        }
+        System.out.println(res);
     }
 
-    public Set<String> degreeOfrelatioship(Relationship re){
-        for(Node t: tree){
-            if(t.re==re){
-                res.add(t.p1.fullName);    
+    public Set<String> degreeOfrelatioship(Relationship re) {
+        for (Node t : tree) {
+            if (t.re == re) {
+                res.add(t.p1.fullName);
             }
         }
         return res;
     }
-                        
-}
 
+}
