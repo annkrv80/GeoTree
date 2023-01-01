3c8964abbe95189b76ab1b3071d2ae7098fb4053
Anna <annkrv80@yandex.ru>
2023-01-01 15:20:17 +0700

    Main

--- Main.java
+++ Main.java
@@ -1,12 +1,12 @@
 public class Main {
     public static void main(String[] args) {
-        Person irina = new Person("Ирина Петрова",30);
-        Person vasya = new Person("Василий Петров",33);
-        Person masha = new Person("Маша Петрова",8);
-        Person lena = new Person("Елена Кошкина",55);
-        Person ivan = new Person("Иван Кошкин",57);
-        Person olga=new Person("Ольга Петрова",60);
-        Person sergey=new Person("Сергей Петров",62);
+        Person irina = new Person("Ирина Петрова", 30);
+        Person vasya = new Person("Василий Петров", 33);
+        Person masha = new Person("Маша Петрова", 8);
+        Person lena = new Person("Елена Кошкина", 55);
+        Person ivan = new Person("Иван Кошкин", 57);
+        Person olga = new Person("Ольга Петрова", 60);
+        Person sergey = new Person("Сергей Петров", 62);
 
         GeoTree gt = new GeoTree();
         gt.appendMother(irina, masha);
@@ -30,5 +30,5 @@ public class Main {
 
         System.out.println(new Reserch(gt).degreeOfrelatioship(Relationship.granny));
     }
-    
+
 }
