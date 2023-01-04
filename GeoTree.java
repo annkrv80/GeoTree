daae30b5ec19aed0734c58a5f667e5ba77b03602
Anna <annkrv80@yandex.ru>
2023-01-04 23:01:38 +0700

    Создание Древа

--- GeoTree.java
+++ GeoTree.java
@@ -8,29 +8,10 @@ public class GeoTree {
         return tree;
     }
 
-    public void appendDad(Person dad, Person children) {
-        tree.add(new Node(dad, Relationship.dad, children));
-        tree.add(new Node(children, Relationship.children, dad));
+    public void append(Person p1, Person p2, Relationship r1, Relationship r2) {
+        tree.add(new Node(p1, r1, p2));
+        tree.add(new Node(p2, r2, p1));
     }
 
-    public void appendMother(Person mother, Person children) {
-        tree.add(new Node(mother, Relationship.mother, children));
-        tree.add(new Node(children, Relationship.children, mother));
-    }
-
-    public void appendGranny(Person granny, Person grandchild) {
-        tree.add(new Node(granny, Relationship.granny, grandchild));
-        tree.add(new Node(grandchild, Relationship.grandchild, granny));
-    }
-
-    public void appendGrandpa(Person grandpa, Person grandchild) {
-        tree.add(new Node(grandpa, Relationship.grandpa, grandchild));
-        tree.add(new Node(grandchild, Relationship.grandchild, grandpa));
-    }
-
-    public void appendSpouse(Person wife, Person husband) {
-        tree.add(new Node(wife, Relationship.spouses, husband));
-        tree.add(new Node(husband, Relationship.spouses, wife));
-    }
-
+    
 }
